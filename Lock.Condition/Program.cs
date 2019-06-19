273f16a90275d296403126732f3e0bf0952873ad
Kiran Jagathlal <kiranjagz@gmail.com>
2019-06-19 21:36:51 +0200

    Added lock example.

---
+++ Lock.Condition/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace Lock.Condition
+{
+    class Program
+    {
+        // shared private fields
+        private static int value1 = 1;
+        private static int value2 = 1;
+
+        #region synchronisation object
+        private static object syncObj = new object();
+        #endregion
+
+        // thread work method
+        public static void DoWork()
+        {
+            lock (syncObj)
+            {
+                if (value2 > 0)
+                {
+                    Console.WriteLine(value1 / value2);
+                    value2 = 0;
+                }
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            // start two threads
+            Thread t1 = new Thread(DoWork);
+            Thread t2 = new Thread(DoWork);
+            t1.Start();
+            t2.Start();
+        }
+    }
+}
