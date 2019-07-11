2d89f1da4d227e78316d0a61ad7ee96505e070d7
Kiran Jagathlal <kiranjagz@gmail.com>
2019-07-11 22:18:50 +0200

    Push block example on form.

---
+++ Async.Demo/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Async.Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBlock = new System.Windows.Forms.Button();
+            this.btnAsync = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnBlock
+            // 
+            this.btnBlock.Location = new System.Drawing.Point(56, 54);
+            this.btnBlock.Name = "btnBlock";
+            this.btnBlock.Size = new System.Drawing.Size(75, 23);
+            this.btnBlock.TabIndex = 0;
+            this.btnBlock.Text = "Block";
+            this.btnBlock.UseVisualStyleBackColor = true;
+            this.btnBlock.Click += new System.EventHandler(this.BtnBlock_Click);
+            // 
+            // btnAsync
+            // 
+            this.btnAsync.Location = new System.Drawing.Point(56, 101);
+            this.btnAsync.Name = "btnAsync";
+            this.btnAsync.Size = new System.Drawing.Size(75, 23);
+            this.btnAsync.TabIndex = 1;
+            this.btnAsync.Text = "Async";
+            this.btnAsync.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 265);
+            this.Controls.Add(this.btnAsync);
+            this.Controls.Add(this.btnBlock);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBlock;
+        private System.Windows.Forms.Button btnAsync;
+    }
+}
+
---
+++ Async.Demo/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Async.Demo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnBlock_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                var fact = FactGet(i, "math");
+                MessageBox.Show(fact);
+            }
+        }
+
+        public string FactGet(int number, string type)
+        {
+            var url = $"http://numbersapi.com/" + number + "/" + type + "?json";
+            using (var client = new HttpClient())
+            {
+                var response = client.GetByteArrayAsync(url).Result;
+                var responseString = Encoding.UTF8.GetString(response);
+                if (!string.IsNullOrWhiteSpace(responseString))
+                {
+                    return responseString;
+                }
+            }
+            return null;
+        }
+    }
+}
