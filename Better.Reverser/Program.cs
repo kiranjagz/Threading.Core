5209ecf4189a01080838392b223db0a69844273a
Kiran Jagathlal <kiranjagz@gmail.com>
2019-07-15 21:41:16 +0200

    Async stuff.

---
+++ Better.Reverser/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Better.Reverser
+{
+    class Program
+    {
+        private static string ReverseString(string s)
+        {
+            Thread.Sleep(1000);
+            StringBuilder sb = new StringBuilder();
+            for (int i = s.Length - 1; i >= 0; i--)
+                sb.Append(s[i]);
+            return sb.ToString();
+        }
+
+        private static string[] Map(string sentence)
+        {
+            return sentence.Split();
+        }
+
+        public static string[] Process(string[] words)
+        {
+            for (int i = 0; i < words.Length; i++)
+            {
+                int index = i;
+                Task.Factory.StartNew(
+                    () => words[index] = ReverseString(words[index]),
+                    TaskCreationOptions.AttachedToParent | TaskCreationOptions.LongRunning);
+            }
+            return words;
+        }
+
+        private static string Reduce(string[] words)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (string word in words)
+            {
+                sb.Append(word);
+                sb.Append(' ');
+            }
+            return sb.ToString();
+        }
+
+        public static void Main(string[] args)
+        {
+            string sentence = "the quick brown fox jumped over the lazy dog";
+
+            // set up map-reduce process
+            var task = Task<string[]>.Factory.StartNew(() => Map(sentence))
+                .ContinueWith<string[]>(t => Process(t.Result))
+                    .ContinueWith<string>(t => Reduce(t.Result));
+
+            // display result
+            Console.WriteLine("Result: {0}", task.Result);
+        }
+    }
+}
---
+++ Word.Reverser/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Word.Reverser
+{
+    class Program
+    {
+        private static List<Task<string>> tasks = new List<Task<string>>();
+
+        private static string ReverseString(string s)
+        {
+            Thread.Sleep(1000);
+            StringBuilder sb = new StringBuilder();
+            for (int i = s.Length - 1; i >= 0; i--)
+                sb.Append(s[i]);
+            return sb.ToString();
+        }
+
+        public static void ProcessSentence(string sentence)
+        {
+            foreach (string word in sentence.Split())
+            {
+                tasks.Add(Task<string>.Factory.StartNew(
+                    () => ReverseString(word) + " ",
+                    TaskCreationOptions.AttachedToParent | TaskCreationOptions.LongRunning));
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            string sentence = "the quick brown fox jumped over the lazy dog";
+
+            // run parent tasks to process sentence
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            Task.Factory.StartNew(() => ProcessSentence(sentence)).Wait();
+            sw.Stop();
+            Console.WriteLine("Total runtime: {0}ms", sw.ElapsedMilliseconds);
+
+            #region verify that all tasks have completed
+            for (int i = 0; i < tasks.Count; i++)
+                Console.WriteLine("Task {0} complete: {1}",
+                                  i, tasks[i].IsCompleted);
+            #endregion
+
+            // display result
+            Console.Write("Result: ");
+            foreach (Task<string> t in tasks)
+                Console.Write(t.Result);
+        }
+    }
+}
