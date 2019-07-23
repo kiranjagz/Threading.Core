71a3c5ad2d4735c3693baee737b2f029b888e523
Kiran Jagathlal <kiranjagz@gmail.com>
2019-07-23 19:21:02 +0200

    Added plinq code.

---
+++ PLINQ.Reverser/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PLINQ.Reverser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var task = Task.Run(() =>
+            {
+                Console.WriteLine("Hello World!");
+                Console.WriteLine("Is background thread: {0}", Thread.CurrentThread.IsBackground);
+                Console.WriteLine("Is threadpool thread: {0}", Thread.CurrentThread.IsThreadPoolThread);
+                Console.WriteLine("Id: {0}", Thread.CurrentThread.ManagedThreadId);
+            });
+
+            var longTask = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine("Hello World Again!");
+                Console.WriteLine("Is background thread: {0}", Thread.CurrentThread.IsBackground);
+                Console.WriteLine("Is threadpool thread: {0}", Thread.CurrentThread.IsThreadPoolThread);
+                Console.WriteLine("Long task Id: {0}", Thread.CurrentThread.ManagedThreadId);
+            },TaskCreationOptions.LongRunning);
+     
+
+            string sentence = "the quick brown fox jumped over the lazy dog ";
+
+            var words =
+                sentence.Split()
+                    .AsParallel()
+                    .AsOrdered()
+                    .WithExecutionMode(ParallelExecutionMode.ForceParallelism)
+                    .Select(word => new string(word.Reverse().ToArray()));
+
+            Console.WriteLine(string.Join(" ", words));
+
+            Console.Read();
+        }
+    }
+}
